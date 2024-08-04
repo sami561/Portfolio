7dddfaf4ea8e5b72a6aa77dc95e78cf533292f5a
sami <samiayachi653@gmail.com>
2024-08-04 15:46:13 +0100

    ✨feature: init commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,15 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
+import PageTransition from "@/components/pageTransition";
+import StairTransition from "@/components/StairTransition";
 
-const inter = Inter({ subsets: ["latin"] });
+const JetBrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  variable: "--font-jetbrainsMono ",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +23,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={JetBrainsMono.variable}>
+        <Header />
+        <StairTransition />
+        <PageTransition> {children}</PageTransition>
+        {children}
+      </body>
     </html>
   );
 }
---
+++ components/Nav.tsx
@@ -0,0 +1,47 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+const links = [
+  {
+    name: "home",
+    path: "/",
+  },
+  {
+    name: "services",
+    path: "/services",
+  },
+  {
+    name: "resume",
+    path: "/resume",
+  },
+  {
+    name: "Work",
+    path: "/work",
+  },
+  {
+    name: "contact",
+    path: "/contact",
+  },
+];
+const Nav = () => {
+  const pathname = usePathname();
+  console.log("🚀 ~ Nav ~ pathname:", pathname);
+
+  return (
+    <nav className="flex gap-8">
+      {links.map((link, index) => (
+        <Link
+          key={index}
+          href={link.path}
+          className={`${
+            link.path === pathname && "text-accent border-b-2 border-accent"
+          } capitalize font-medium hover:text-accent transition-all  `}
+        >
+          {link.name}
+        </Link>
+      ))}
+    </nav>
+  );
+};
+
+export default Nav;
